4106a410c2ffcd7deb15479f573c303098749164
DESKTOP-PLSIG0T\LSHSMD666 <1107863026@qq.com>
2017-05-16 14:16:23 +0800

    退后四个版本

--- demo/src/main/java/com/yaorange/DemoController.java
+++ demo/src/main/java/com/yaorange/DemoController.java
@@ -3,5 +3,7 @@ package com.yaorange;
 public class DemoController {
 	public static void main(String[] args) {
 		System.out.println("1111");
+		System.out.println("master未更新添加");
+		System.out.println("lisi");
 	}
 }
